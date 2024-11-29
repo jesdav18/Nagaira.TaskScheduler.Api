89c6da609dcd0ad9153cc1e8510da8e1561be5b0
Jesuar López <jesdav18@gmail.com>
2024-11-28 22:23:25 -0600

    Update Startup.cs

--- Nagaira.TaskScheduler.Api/Startup.cs
+++ Nagaira.TaskScheduler.Api/Startup.cs
@@ -115,8 +115,8 @@ namespace Nagaira.TaskScheduler.Api
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    var ambiente = env.IsProduction() ? "Producción papa ssiuuuuuuu" : "Desarrollo";
-                    await context.Response.WriteAsync($"<b>Nagaira TaskScheduler Api | {ambiente}</b><hr>");
+                    var ambiente = env.IsProduction() ? "Producción" : "Desarrollo";
+                    await context.Response.WriteAsync($"<b>Nagaira TaskScheduler Api | {ambiente} Siuuuuuuu</b><hr>");
                 });
                 endpoints.MapControllers();
             });
